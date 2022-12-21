303cf9a78c193e923797b4971578c746a04d3cb8
Guilherme Britto <guilhermefragab@gmail.com>
2022-12-21 05:07:11 -0300

    done

--- pages/script/jobsData.js
+++ pages/script/jobsData.js
@@ -28,13 +28,4 @@ const jobsData = [
   },
 ];
 
-let selected = [
-  {
-    id: 2,
-    title: "Pessoa desenvolvedora Fullstack - Node JS",
-    enterprise: "Brazilians in Tech",
-    location: "Rio de Janeiro",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Home Office", "Presencial"],
-  }]
+let selected = []
